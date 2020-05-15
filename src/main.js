0ce9e7f48d04451149b4a0b558cdd5fcb0094350
David Spörri <spoerridavid@gmail.com>
2020-05-15 22:25:58 +0200

    install bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
