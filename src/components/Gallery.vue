21bbc1dc874b1e4687df7dc48c1ae81506767864
David Spörri <spoerridavid@gmail.com>
2020-05-17 18:55:31 +0200

    add component

---
+++ src/components/Gallery.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'gallery',
+  created: function() {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
