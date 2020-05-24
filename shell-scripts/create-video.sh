740256bb8abb1eb93139005c4da01273633ca48d
David Spörri <spoerridavid@gmail.com>
2020-05-24 13:36:02 +0200

    always run ffmpeg in video directory

--- shell-scripts/create-video.sh
+++ shell-scripts/create-video.sh
@@ -2,6 +2,7 @@
 # Workaround to ffmpeg bug: https://github.com/snapcrafters/ffmpeg/issues/41#issuecomment-533625390
 echo 'creating video'
 echo $(date +%Y-%m-%d-%H-%M)
+cd $(dirname "$0")
 
 #cat *.jpeg | ffmpeg -i pipe:0 -r 24 -f matroska pipe:1 | cat > output.avi
 cat *.jpeg | ffmpeg -i pipe:0 -r 24 -c:v libvpx-vp9 -b:v 2M -f webm pipe:1 | cat > output.webm
